7fe7e33716f2410980cdf76a9fb4f778a7a5880f
Rene Hexel <rhx@users.noreply.github.com>
2022-05-22 08:13:25 +1000

    Add a wrapper for pcap_if_t

---
+++ Sources/swift-pcap/Interface.swift
@@ -0,0 +1,55 @@
+//
+//  Interface.swift
+//  
+//  Copyright (c) 2022 Rene Hexel, all rights reserved.
+//  Created by Rene Hexel on 22/5/2022.
+//
+import CLibPCap
+
+/// Representation of a network interface
+public struct Interface: CustomStringConvertible {
+    /// Interface flags
+    public struct Flags: OptionSet {
+        /// The underlying flags as a bit set
+        public let rawValue: Int32
+        /// Designated initialiser
+        @inlinable
+        public init(rawValue: Int32 = 0) {
+            self.rawValue = rawValue
+        }
+        /// Interface is a loopback interface
+        public static let loopback = Flags(rawValue: PCAP_IF_LOOPBACK)
+        /// Interface is up
+        public static let up = Flags(rawValue: PCAP_IF_UP)
+        /// Interface is running
+        public static let running = Flags(rawValue: PCAP_IF_RUNNING)
+        /// Interface is a wireless interface
+        public static let wireless = Flags(rawValue: PCAP_IF_WIRELESS)
+    }
+    /// A pointer to the underlying PCAP interface type
+    public var pcapInterface: UnsafeMutablePointer<pcap_if_t>
+
+    /// Designated initialiser
+    /// - Parameter interface: The interface pointer to wrap
+    @inlinable
+    public init(_ interface: UnsafeMutablePointer<pcap_if_t>) {
+        pcapInterface = interface
+    }
+
+    /// A textual description of the interface
+    /// - Note: The description can be empty
+    @inlinable
+    public var description: String {
+        pcapInterface.pointee.description.map {
+            String(cString: $0)
+        } ?? ""
+    }
+
+    /// The name of the interface
+    @inlinable
+    public var name: String {
+        pcapInterface.pointee.name.map {
+            String(cString: $0)
+        } ?? ""
+    }
+}
