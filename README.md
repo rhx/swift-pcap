0c98d33c2cb0df2aefd34754abf98491dade3382
Rene Hexel <rhx@users.noreply.github.com>
2022-05-21 17:28:05 +1000

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-pcap
+
+A description of this package.
---
+++ Tests/swift-pcapTests/swift_pcapTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_pcap
+
+final class swift_pcapTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_pcap().text, "Hello, World!")
+    }
+}
