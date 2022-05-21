2ec176225ccc282e03648018a970c328f1787a05
Rene Hexel <rhx@users.noreply.github.com>
2022-05-22 09:16:45 +1000

    Add flags and next properties

--- Sources/swift-pcap/Interface.swift
+++ Sources/swift-pcap/Interface.swift
@@ -11,20 +11,25 @@ public struct Interface: CustomStringConvertible {
     /// Interface flags
     public struct Flags: OptionSet {
         /// The underlying flags as a bit set
-        public let rawValue: Int32
+        public let rawValue: UInt32
         /// Designated initialiser
         @inlinable
-        public init(rawValue: Int32 = 0) {
+        public init(rawValue: UInt32 = 0) {
             self.rawValue = rawValue
         }
+        /// Signed value initialiser
+        @inlinable
+        public init(_ signed: Int32) {
+            rawValue = .init(bitPattern: signed)
+        }
         /// Interface is a loopback interface
-        public static let loopback = Flags(rawValue: PCAP_IF_LOOPBACK)
+        public static let loopback = Flags(PCAP_IF_LOOPBACK)
         /// Interface is up
-        public static let up = Flags(rawValue: PCAP_IF_UP)
+        public static let up = Flags(PCAP_IF_UP)
         /// Interface is running
-        public static let running = Flags(rawValue: PCAP_IF_RUNNING)
+        public static let running = Flags(PCAP_IF_RUNNING)
         /// Interface is a wireless interface
-        public static let wireless = Flags(rawValue: PCAP_IF_WIRELESS)
+        public static let wireless = Flags(PCAP_IF_WIRELESS)
     }
     /// A pointer to the underlying PCAP interface type
     public var pcapInterface: UnsafeMutablePointer<pcap_if_t>
@@ -34,6 +39,12 @@ public struct Interface: CustomStringConvertible {
     @inlinable
     public init(_ interface: UnsafeMutablePointer<pcap_if_t>) {
         pcapInterface = interface
+    }
+
+    /// Interface flags
+    @inlinable
+    public var flags: Flags {
+        Flags(rawValue: pcapInterface.pointee.flags)
     }
 
     /// A textual description of the interface
@@ -51,5 +62,11 @@ public struct Interface: CustomStringConvertible {
         pcapInterface.pointee.name.map {
             String(cString: $0)
         } ?? ""
+    }
+
+    /// The next interface in the list
+    @inlinable
+    public var next: Interface? {
+        pcapInterface.pointee.next.map(Interface.init)
     }
 }
