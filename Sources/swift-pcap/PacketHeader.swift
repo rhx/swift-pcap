32819c4b99c4ac1e3010c3c26be38b0bddc87362
Rene Hexel <rhx@users.noreply.github.com>
2022-05-22 13:16:18 +1000

    Make PacketHeader wrap struct rather than pointer

--- Sources/swift-pcap/PacketHeader.swift
+++ Sources/swift-pcap/PacketHeader.swift
@@ -8,39 +8,46 @@ import CLibPCap
 
 /// Representation of a packet header interface
 public struct PacketHeader: CustomStringConvertible {
-    /// A pointer to the underlying PCAP address type
-    public let pcapHeader: UnsafePointer<pcap_pkthdr>
+    /// The underlying PCAP header
+    public var pcapHeader = pcap_pkthdr()
 
     /// Designated initialiser
-    /// - Parameter header: The header pointer to wrap
+    @inlinable
+    public init() {}
+
+    /// Copying initialiser
+    /// - Parameter header: Pointer to the header to copy
     @inlinable
     public init(_ header: UnsafePointer<pcap_pkthdr>) {
-        pcapHeader = header
+        pcapHeader = header.pointee
     }
 
     /// A textual description of the address
     @inlinable
     public var description: String {
-        pcapHeader.withMemoryRebound(to: CChar.self, capacity: MemoryLayout<pcap_pkthdr>.size) {
-            String(cString: $0 + MemoryLayout<pcap_pkthdr>.offset(of: \.comment)!)
+        var hdr = pcapHeader
+        return withUnsafePointer(to: &hdr) {
+            $0.withMemoryRebound(to: CChar.self, capacity: MemoryLayout<pcap_pkthdr>.size) {
+                String(cString: $0 + MemoryLayout<pcap_pkthdr>.offset(of: \.comment)!)
+            }
         }
     }
 
     /// Length of the portion present
     @inlinable
     public var count: Int {
-        Int(pcapHeader.pointee.caplen)
+        Int(pcapHeader.caplen)
     }
 
     /// Size of this packet (off-wire)
     @inlinable
     public var size: Int {
-        Int(pcapHeader.pointee.len)
+        Int(pcapHeader.len)
     }
 
     /// Time stamp
     @inlinable
     public var timeStamp: timeval {
-        pcapHeader.pointee.ts
+        pcapHeader.ts
     }
 }
